77de4987f5670aa5d09668a12d3b5915e7af0225
Nikita webconn Maslov <n.maslov@contactless.ru>
2016-06-16 19:49:24 +0300

    pulsar: ReadRegister() method complete
    Signed-off-by: Nikita webconn Maslov <n.maslov@contactless.ru>

--- pulsar_device.h
+++ pulsar_device.h
@@ -6,6 +6,7 @@
 #include "serial_device.h"
 #include <stdint.h>
 
+
 class TPulsarDevice: public TSerialDevice {
 public:
 
@@ -21,10 +22,19 @@ public:
 private:
     void WriteBCD(uint64_t data, uint8_t *buffer, size_t size, bool big_endian = true);
     void WriteHex(uint64_t data, uint8_t *buffer, size_t size, bool big_endian = true);
+
+    uint64_t ReadBCD(const uint8_t *data, size_t size, bool big_endian = true);
+    uint64_t ReadHex(const uint8_t *data, size_t size, bool big_endian = true);
+
     uint16_t CalculateCRC16(const uint8_t *buffer, size_t size);
 
     void WriteDataRequest(uint32_t addr, uint32_t mask, uint16_t id);
     void WriteSysTimeRequest(uint32_t addr, uint16_t id);
+
+    void ReadResponse(uint32_t addr, uint8_t *payload, size_t size, uint16_t id);
+    
+    uint64_t ReadDataRegister(PRegister reg);
+    uint64_t ReadSysTimeRegister(PRegister reg);
 };
 
 typedef std::shared_ptr<TPulsarDevice> PPulsarDevice;
