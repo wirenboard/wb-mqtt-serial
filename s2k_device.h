5e043efba00a86ac5f9de3719ac7d4d118999af1
Yury Usishchev <yury.usischev@phystech.edu>
2016-11-09 18:09:34 +0300

    S2K device: style fixes

--- s2k_device.h
+++ s2k_device.h
@@ -9,25 +9,22 @@
 
 class TS2KDevice: public TBasicProtocolSerialDevice<TBasicProtocol<TS2KDevice>> {
 public:
-    static const int DefaultTimeoutMs = 1000;
-    enum RegisterType {
+    enum RegisterType
+    {
         REG_RELAY = 0,
-	REG_RELAY_MULTI,
-	REG_RELAY_DEFAULT,
-	REG_RELAY_DELAY,
-	REG_ADDRESS,
-	REG_CASE,
-	REG_VOLTAGE
+        REG_RELAY_MULTI,
+        REG_RELAY_DEFAULT,
+        REG_RELAY_DELAY,
     };
 
     TS2KDevice(PDeviceConfig config, PAbstractSerialPort port, PProtocol protocol);
-    uint64_t ReadRegister(PRegister reg);
-    void WriteRegister(PRegister reg, uint64_t value);
+    virtual uint64_t ReadRegister(PRegister reg);
+    virtual void WriteRegister(PRegister reg, uint64_t value);
 
 private:
-    uint8_t S2KCrc(const uint8_t *array, int size);
-    static uint8_t crc_table[];
-    uint8_t relay_state[5];
+    uint8_t CrcS2K(const uint8_t *array, int size);
+    static uint8_t CrcTable[];
+    uint8_t RelayState[5];
 };
 
 typedef std::shared_ptr<TS2KDevice> PS2KDevice;
