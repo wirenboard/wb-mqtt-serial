241b83a22a8d6338d8311468ced33a64c52c1275
Evgeny Boger <boger@contactless.ru>
2018-12-26 00:59:24 +0300

    fix u32 slave_id string parser (#77)
    default slave_id  is now unsigned long instead of int.
    It means that values greater than 0xF0000000 are now correctly
    parsed. Test is added to mercury200 where the problem was first
    reported.

--- test/mercury200_expectations.cpp
+++ test/mercury200_expectations.cpp
@@ -4,21 +4,21 @@ void TMercury200Expectations::EnqueueMercury200EnergyResponse()
 {
     Expector()->Expect(
         {
-            0x01, // addr
+            0xFE, // addr
+            0x12, // addr
+            0x34, // addr
             0x56, // addr
-            0x10, // addr
-            0x1e, // addr
             0x27,
-            0x40,
-            0x37
+            0x36,
+            0xd8
         },
         {
-            0x01, 0x56, 0x10, 0x1e, 0x27, // request
+            0xFE, 0x12, 0x34, 0x56, 0x27, // request
             0x00, 0x06, 0x21, 0x42, // tariff1
             0x00, 0x02, 0x08, 0x34, // tariff2
             0x00, 0x01, 0x11, 0x11, // tariff3
             0x00, 0x02, 0x22, 0x22, // tariff4
-            0x22, 0x2f //crc
+            0x2b, 0x25 //crc
         }, __func__);
 }
 
@@ -26,20 +26,20 @@ void TMercury200Expectations::EnqueueMercury200ParamResponse()
 {
     Expector()->Expect(
         {
-            0x01, // addr
+            0xfe, // addr
+            0x12, // addr
+            0x34, // addr
             0x56, // addr
-            0x10, // addr
-            0x1e, // addr
             0x63,
-            0x40,
-            0x04
+            0x36,
+            0xeb
         },
         {
-            0x01, 0x56, 0x10, 0x1e, 0x63, // request
+            0xFE, 0x12, 0x34, 0x56, 0x63, // request
             0x12, 0x34, // Voltage
             0x56, 0x78, // Current
             0x76, 0x54, 0x32, // Power
-            0x78, 0x56 //crc
+            0x79, 0x0e //crc
         }, __func__);
 }
 
@@ -47,17 +47,17 @@ void TMercury200Expectations::EnqueueMercury200BatteryVoltageResponse()
 {
     Expector()->Expect(
         {
-            0x01, // addr
+            0xFE, // addr
+            0x12, // addr
+            0x34, // addr
             0x56, // addr
-            0x10, // addr
-            0x1e, // addr
             0x29, // read battery Voltage
-            0xc1,
-            0xf3
+            0xb7,
+            0x1c
         },
         {
-            0x01, 0x56, 0x10, 0x1e, 0x29, // addr + cmd
+            0xFE, 0x12, 0x34, 0x56, 0x29, // addr + cmd
             0x03, 0x91, // Voltage
-            0xd0, 0x89 //crc
+            0xb7, 0x65 //crc
         }, __func__);
 }
