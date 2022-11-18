f9a059c2fafcf57553242b55ba97366509cf1d69
Roman Kochkin <roman.kochkin@wirenboard.ru>
2022-11-18 10:17:08 +0300

    Added comment to ConvertToMqttTopicValidString() method

--- src/common_utils.h
+++ src/common_utils.h
@@ -3,5 +3,8 @@
 
 namespace util
 {
+    /// Converts a string to a string suitable for use in mqtt topics
+    /// \param src original string
+    /// \return modified string
     std::string ConvertToMqttTopicValidString(const std::string& src);
 }
