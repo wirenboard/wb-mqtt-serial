2c90cde3180becaa03fadbaf51536dfa030a5026
Petr Krasnoshchekov <petr.krasnoshchekov@wirenboard.ru>
2023-03-16 09:54:35 +0000

    Enable events implementation

--- src/modbus_ext_common.h
+++ src/modbus_ext_common.h
@@ -4,6 +4,14 @@
 
 namespace ModbusExt // modbus extension protocol common utilities
 {
+    enum TEventRegisterType : uint8_t
+    {
+        COIL = 1,
+        DISCRETE = 2,
+        HOLDING = 3,
+        INPUT = 4
+    };
+
     class IEventsVisitor
     {
     public:
@@ -21,5 +29,33 @@ namespace ModbusExt // modbus extension protocol common utilities
                     const std::chrono::milliseconds& responseTimeout,
                     const std::chrono::milliseconds& frameTimeout,
                     IEventsVisitor& eventVisitor);
+
+    class TEventsEnabler
+    {
+    public:
+        typedef std::function<void(uint16_t, TEventRegisterType, uint8_t)> TVisitorFn;
+
+        TEventsEnabler(uint8_t slaveId,
+                       TPort& port,
+                       const std::chrono::milliseconds& responseTimeout,
+                       const std::chrono::milliseconds& frameTimeout,
+                       TEventsEnabler::TVisitorFn visitor);
+
+        void Enable(uint16_t addr, TEventRegisterType type);
+        void Finalize();
+
+    private:
+        std::vector<uint8_t> Request;
+        std::vector<uint8_t> Response;
+
+        uint8_t SlaveId;
+        TPort& Port;
+        const std::chrono::milliseconds& ResponseTimeout;
+        const std::chrono::milliseconds& FrameTimeout;
+        TVisitorFn Visitor;
+
+        void EnableEvents();
+        void ClearRequest();
+    };
 
 } // modbus extension protocol common utilities
