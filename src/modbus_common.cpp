812ea48f3c1d0546a0e58b6271a8288c1d3e3267
Petr Krasnoshchekov <petr.krasnoshchekov@wirenboard.ru>
2023-04-27 07:53:30 +0000

    Fix first reading of a register with events

--- src/modbus_common.cpp
+++ src/modbus_common.cpp
@@ -131,7 +131,7 @@ namespace Modbus // modbus protocol common utilities
 
     bool TModbusRegisterRange::Add(PRegister reg, std::chrono::milliseconds pollLimit)
     {
-        if (reg->IsExcludedFromPolling()) {
+        if (reg->GetAvailable() == TRegisterAvailability::UNAVAILABLE) {
             return true;
         }
 
