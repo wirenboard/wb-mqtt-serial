934d6c84f1729315c68b5723a35b2496354b4163
Petr Krasnoshchekov <petr.krasnoshchekov@wirenboard.ru>
2023-12-07 11:15:08 +0500

    Fix formatting

--- src/bin_utils.cpp
+++ src/bin_utils.cpp
@@ -3,7 +3,7 @@
 
 uint64_t BinUtils::GetLSBMask(uint8_t bitCount)
 {
-    if(bitCount < sizeof(uint64_t) * 8) {
+    if (bitCount < sizeof(uint64_t) * 8) {
         return (uint64_t(1) << bitCount) - 1;
     }
     return 0xFFFFFFFFFFFFFFFF;
