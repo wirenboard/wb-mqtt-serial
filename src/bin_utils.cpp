00b4360e7e3e8693088caa533d92d1ee21065331
Petr Krasnoshchekov <petr.krasnoshchekov@wirenboard.ru>
2023-12-07 11:12:06 +0500

    Fix arm64 build

--- src/bin_utils.cpp
+++ src/bin_utils.cpp
@@ -3,6 +3,8 @@
 
 uint64_t BinUtils::GetLSBMask(uint8_t bitCount)
 {
-    assert(bitCount <= sizeof(uint64_t) * 8);
-    return (uint64_t(1) << bitCount) - 1;
+    if(bitCount < sizeof(uint64_t) * 8) {
+        return (uint64_t(1) << bitCount) - 1;
+    }
+    return 0xFFFFFFFFFFFFFFFF;
 }
