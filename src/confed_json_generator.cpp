fcac537ff330b21bc8afb72b63685586d4a5317e
Petr Krasnoshchekov <petr.krasnoshchekov@wirenboard.ru>
2024-03-29 11:42:00 +0500

    Fix formatting

--- src/confed_json_generator.cpp
+++ src/confed_json_generator.cpp
@@ -325,7 +325,7 @@ namespace
 Json::Value MakeJsonForConfed(const std::string& configFileName, TTemplateMap& templates)
 {
     Json::Value root(Parse(configFileName));
-    // If a file contains some symbols, but not a valid JSON, 
+    // If a file contains some symbols, but not a valid JSON,
     // jsoncpp can return Json::nullValue instead of throwing an error
     if (!root.isObject()) {
         throw std::runtime_error("Failed to parse " + configFileName + ". The file is not a valid JSON");
