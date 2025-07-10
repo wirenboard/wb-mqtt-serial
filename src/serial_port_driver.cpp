ae1d73035761088b18bcffeb40b1e8e4301d300c
u236 <mail@u236.org>
2025-07-10 09:58:16 +0300

    fix typo

--- src/serial_port_driver.cpp
+++ src/serial_port_driver.cpp
@@ -144,7 +144,7 @@ void TSerialPortDriver::OnValueRead(PRegister reg)
         return;
     }
     if (it->second->HasValuesOfAllRegisters()) {
-        // change publis policy for sporadic registers to publish register data on every read, even if it not changed
+        // change publish policy for sporadic registers to publish register data on every read, even if it not changed
         // needed for DALI bus devices polling
         auto publishPolicy = PublishPolicy;
         if ((reg->GetConfig()->SporadicMode == TRegisterConfig::TSporadicMode::ONLY_EVENTS &&
