746d921c4ac7494ddfb723b959b5afce8dd97ccf
KraPete <86825564+KraPete@users.noreply.github.com>
2026-09-14 14:35:49 +0500

    Fix writing to devices which have no registers to poll (#1259)

--- src/serial_device.h
+++ src/serial_device.h
@@ -281,6 +281,9 @@ public:
     std::chrono::steady_clock::time_point GetLastReadTime() const;
     void SetLastReadTime(std::chrono::steady_clock::time_point readTime);
 
+    std::chrono::steady_clock::time_point GetLastWriteTime() const;
+    void SetLastWriteTime(std::chrono::steady_clock::time_point writeTime);
+
     void AddOnConnectionStateChangedCallback(TDeviceCallback callback);
 
     PRegister GetSnRegister() const;
@@ -316,6 +319,7 @@ private:
 
     std::list<PRegister> Registers;
     std::chrono::steady_clock::time_point LastReadTime;
+    std::chrono::steady_clock::time_point LastWriteTime;
     std::vector<TDeviceCallback> ConnectionStateChangedCallbacks;
     PRegister SnRegister;
 
